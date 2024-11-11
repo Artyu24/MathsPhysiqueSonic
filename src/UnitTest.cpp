d50cdb0affd26e19c06ed2555a35eae9c4ed7986
Artyu24 <65313164+Artyu24@users.noreply.github.com>
2024-11-11 00:03:38 -0500

    [ADD] More Matrix Unit Test

--- src/UnitTest.cpp
+++ src/UnitTest.cpp
@@ -86,47 +86,268 @@ void UnitTest::MatrixTest() const
 	std::cout << "Unit Test Matrix : Start" << std::endl;
 
 	//Constructor Test
-	Matrix m(3, 3);
-	assert(m.NbLines() == 3);
-	assert(m.NbColumns() == 3);
+	Matrix m1(3, 3);
+	assert(m1.NbLines() == 3);
+	assert(m1.NbColumns() == 3);
 
-	m = Matrix(2, 2, std::vector<float>
+	m1 = Matrix(2, 2, std::vector<float>
 	{
 		1, 2,
 		3, 4
 	});
-	assert(m.Get(0, 0) == 1);
-	assert(m.Get(0, 1) == 2);
-	assert(m.Get(1, 0) == 3);
-	assert(m.Get(1, 1) == 4);
+	assert(m1.Get(0, 0) == 1);
+	assert(m1.Get(0, 1) == 2);
+	assert(m1.Get(1, 0) == 3);
+	assert(m1.Get(1, 1) == 4);
 
 	//Modification Test
-	m = Matrix(2, 2);
-	m.Get(0, 0) = 1;
-	m.Get(0, 1) = 2;
-	m.Get(1, 0) = 3;
-	m.Get(1, 1) = 4;
-	assert(m.Get(0, 0) == 1);
-	assert(m.Get(0, 1) == 2);
-	assert(m.Get(1, 0) == 3);
-	assert(m.Get(1, 1) == 4);
+	m1 = Matrix(2, 2);
+	m1.Get(0, 0) = 1;
+	m1.Get(0, 1) = 2;
+	m1.Get(1, 0) = 3;
+	m1.Get(1, 1) = 4;
+	assert(m1.Get(0, 0) == 1);
+	assert(m1.Get(0, 1) == 2);
+	assert(m1.Get(1, 0) == 3);
+	assert(m1.Get(1, 1) == 4);
 
 	//Copy Test
-	m = Matrix(2, 2, std::vector<float>
+	m1 = Matrix(2, 2, std::vector<float>
 	{
 		1, 2,
 		3, 4
 	});
-	Matrix m2(m);
+	Matrix m2(m1);
 	m2.Get(0, 0) = 23;
-	assert(m.Get(0, 0) == 1);
+	assert(m1.Get(0, 0) == 1);
 	assert(m2.Get(0, 0) == 23);
-	assert(m.Get(0, 1) == 2);
+	assert(m1.Get(0, 1) == 2);
 	assert(m2.Get(0, 1) == 2);
 
 	//Identity
-	m = Matrix::Identity(2);
-	//assert(m.ToArray2D() == std::vector<float>{1.f, 0.f, 1.f, 0.f})
+	m1 = Matrix::Identity(2);
+	std::vector<float> vTest{ 1.f, 0.f, 0.f, 1.f };
+	assert(m1.ToArray2D() == vTest);
+
+	m2 = Matrix(3, 2);
+	assert(m1.IsIdentity() == true);
+	assert(m2.IsIdentity() == false);
+
+	//Scalar Multiplication
+	m1 = Matrix(2, 2, std::vector<float>
+	{
+		1, 2,
+		3, 4
+	});
+	m2 = m1 * 2;
+	vTest = { 2.f, 4.f, 6.f, 8.f };
+	assert(m2.ToArray2D() == vTest);
+
+	m1.Multiply(2);
+	assert(m1.ToArray2D() == vTest);
+
+	//Addition
+	m1 = Matrix(2, 2, std::vector<float>
+	{
+		1, 2,
+		3, 4
+	});
+
+	m2 = Matrix(2, 2, std::vector<float>
+	{
+		5, 6,
+		7, 8
+	});
+
+	Matrix m3 = m1 + m2;
+
+	vTest = { 6.f, 8.f, 10.f, 12.f };
+	assert(m3.ToArray2D() == vTest);
+
+	//Substract
+	m1 = Matrix(2, 2, std::vector<float>
+	{
+		5, 6,
+		7, 8
+	});
+
+	m2 = Matrix(2, 2, std::vector<float>
+	{
+		1, 2,
+		3, 4
+	});
+
+	m3 = m1 - m2;
+
+	vTest = { 4.f, 4.f, 4.f, 4.f };
+	assert(m3.ToArray2D() == vTest);
+
+	//Matrice Multiplication
+	m1 = Matrix(4, 2, std::vector<float>
+	{
+		1,4,
+		2,2,
+		7,6,
+		23,1
+	});
+
+	m2 = Matrix(2, 3, std::vector<float>
+	{
+		8,5,2,
+		3,1,2
+	});
+
+	m3 = m1 * m2;
+	vTest = { 20,9,10,22,12,8,74,41,26,187,116,48 };
+	assert(m3.ToArray2D() == vTest);
+
+	//Transpose
+	m1 = Matrix(2, 3, std::vector<float>
+	{
+		1, 2, 3,
+		4, 5, 6
+	});
+	m1 = Matrix::Transpose(m1);
+
+	vTest = { 1, 4, 2, 5, 3, 6};
+	assert(m1.ToArray2D() == vTest);
+
+	//Swap Line / Column
+	m1 = Matrix(3, 3, std::vector<float>
+	{
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 9
+	});
+	Matrix::SwapLines(m1, 0, 2);
+	vTest = { 7, 8, 9, 4, 5, 6, 1, 2, 3 };
+
+	assert(m1.ToArray2D() == vTest);
+
+	m1 = Matrix(3, 3, std::vector<float>
+	{
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 9
+	});
+	Matrix::SwapColumns(m1, 0, 1);
+	vTest = { 2, 1, 3, 5, 4, 6, 8, 7, 9 };
+
+	assert(m1.ToArray2D() == vTest);
+
+	//Multiply Line / Column
+	m1 = Matrix(3, 3, std::vector<float>
+	{
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 9
+	});
+	Matrix::MultiplyLine(m1, 1, 2);
+	vTest = { 1, 2, 3, 8, 10, 12, 7, 8, 9 };
+
+	assert(m1.ToArray2D() == vTest);
+
+	m1 = Matrix(3, 3, std::vector<float>
+	{
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 9
+	});
+	Matrix::MultiplyColumn(m1, 0, 2);
+	vTest = { 2, 2, 3, 8, 5, 6, 14, 8, 9 };
+
+	assert(m1.ToArray2D() == vTest);
+
+	//Add Line / Column to another
+	m1 = Matrix(3, 3, std::vector<float>
+	{
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 9
+	});
+	Matrix::AddLineToAnother(m1, 1, 0, 2);
+	vTest = { 9, 12, 15, 4, 5, 6, 7, 8, 9 };
+
+	assert(m1.ToArray2D() == vTest);
+
+	m1 = Matrix(3, 3, std::vector<float>
+	{
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 9
+	});
+	Matrix::AddColumnToAnother(m1, 1, 0, 2);
+	vTest = { 5, 2, 3, 14, 5, 6, 23, 8, 9 };
+
+	assert(m1.ToArray2D() == vTest);
+
+	//Generate Augmented Matrix
+	m1 = Matrix(3, 3, std::vector<float>
+	{
+		3, 2, -3,
+		4, -3, 6,
+		1, 0, -1
+	});
+
+	m2 = Matrix(3, 1, std::vector<float>
+	{
+		-13,
+		7,
+		-5
+	});
+
+	m3 = Matrix::GenerateAugmentedMatrix(m1, m2);
+	vTest = { 3,2,-3,-13,4,-3,6,7,1,0,-1,-5 };
+
+	assert(m3.ToArray2D() == vTest);
+
+	//Split Matrix
+	m1 = Matrix(3, 4, std::vector<float>
+	{
+		3, 2, -3, 0,
+		4, -3, 6, 0,
+		1, 0, -1, 0
+	});
+	std::tuple<Matrix, Matrix> mtuple = m1.Split(2);
+
+	vTest = { 3,2,-3, 4,-3,6,1,0,-1};
+	assert(std::get<0>(mtuple).ToArray2D() == vTest);
+
+	vTest = { 0,0,0};
+	assert(std::get<1>(mtuple).ToArray2D() == vTest);
+
+	//Apply Row Reduction
+	//m1 = Matrix(3, 3, std::vector<float>
+	//{
+	//	3, 2, -3,
+	//	4, -3, 6,
+	//	1, 0, -1
+	//});
+	//m2 = Matrix(3, 1, std::vector<float>
+	//{
+	//	-13,
+	//	7,
+	//	-5
+	//});
+	//mtuple = Matrix::ApplyMatrixRowReduction(m1, m2);
+
+	/*vTest = { 1.f,0.f,0.f,0.f,1.f,0.f,0.f,0.f,1.f };
+	assert(std::get<0>(mtuple).ToArray2D() == vTest);*/
+
+	//vTest = { -2,1,3 };
+	//assert(std::get<1>(mtuple).ToArray2D() == vTest);
+
+	//Invert Matrix
+	/*m1 = Matrix(2, 2, std::vector<float>
+	{
+		1, 2,
+		3, 4
+	});
+
+	m1 = Matrix::InvertByRowReduction(m1);
+	vTest = { -2.f, 1.f, 1.5f, -0.5f };
+
+	assert(m1.ToArray2D() == vTest);*/
 
 	std::cout << "Unit Test Matrix : Confirm" << std::endl;
 }
